96009644d41767507e31b5f8536bddb6a658a0b2
Khushboo Thakur <65019143+khushboothakur2305@users.noreply.github.com>
2021-01-23 23:35:47 +0530

    Request & Response

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method);
+  res.setHeader("Content-type", "text/html");
+  res.write(
+    "<html><head>My first Server</head><body><h1>Hello from my first NodeJs server</h1></body></html>"
+  );
+  res.end();
 });
 server.listen(3000);
